17b723f4e29ab8d387dad9b8676d5f52c8e0f206
Joseveji96 <joseveji96@gmail.com>
2025-05-31 00:21:59 -0600

    Feat: add image optimization configuration for unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators : false,
+  images: {
+      unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
